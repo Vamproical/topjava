3e785fbfa0bb9a88da2c96567d5d168dcba162d6
Vamproical <mimoun136@yandex.ru>
2023-02-17 12:21:17 +1000

    add meal and meal date to initDB, populateDB.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_role;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +11,14 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2020-01-30 10:00:00','user breakfast', 500, 100000),
+       ('2020-01-30 13:00:00','user lunch', 1000, 100000),
+       ('2020-01-30 20:00:00','user dinner', 500, 100000),
+       ('2020-01-31 0:00:00','user cheat-mil', 100, 100000),
+       ('2020-01-31 10:00:00','user breakfast', 1000, 100000),
+       ('2020-01-31 13:00:00','user lunch', 500, 100000),
+       ('2020-01-31 20:00:00','user dinner', 410, 100000),
+       ('2020-01-30 10:00:00','admin breakfast', 500, 100001),
+       ('2020-01-30 13:00:00','admin lunch', 500, 100001);
