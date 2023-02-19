6ea41bc0baaed64e46ec24e7a2e02e2f6c1ed2f6
Vamproical <mimoun136@yandex.ru>
2023-02-20 09:39:52 +1000

    A little fix.

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -15,7 +15,7 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
-        "classpath:spring-test.xml"
+        "classpath:spring/spring-test.xml"
 })
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
