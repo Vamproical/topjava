4e119304e68f5dce6ab17444e231db982528350d
Vamproical <mimoun136@yandex.ru>
2023-04-06 18:33:52 +1000

    A little refactoring

--- src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
@@ -2,12 +2,15 @@ package ru.javawebinar.topjava.util;
 
 
 import org.springframework.core.NestedExceptionUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
+import org.springframework.validation.BindingResult;
 import ru.javawebinar.topjava.HasId;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.*;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ValidationUtil {
 
@@ -64,6 +67,13 @@ public class ValidationUtil {
         } else if (bean.id() != id) {
             throw new IllegalArgumentException(bean + " must be with id=" + id);
         }
+    }
+
+    public static ResponseEntity<String> getError(BindingResult result) {
+        String errorFieldsMsg = result.getFieldErrors().stream()
+                .map(fe -> String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
+                .collect(Collectors.joining("<br>"));
+        return ResponseEntity.unprocessableEntity().body(errorFieldsMsg);
     }
 
     //  https://stackoverflow.com/a/65442410/548473
