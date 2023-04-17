8e85c942bf64425421c7132426efe277abe2338c
Vamproical <mimoun136@yandex.ru>
2023-04-17 19:43:18 +1000

    remove redundant variable

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -1,13 +1,11 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
-import ru.javawebinar.topjava.web.ExceptionInfoHandler;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
@@ -17,9 +15,6 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/profile/meals", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MealUIController extends AbstractMealController {
-    @Autowired
-    private ExceptionInfoHandler exceptionInfoHandler;
-
     @Override
     @GetMapping
     public List<MealTo> getAll() {
